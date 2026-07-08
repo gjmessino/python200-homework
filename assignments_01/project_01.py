3f833e3b2dc5df0a24be2a25f00e65200a50c4ff
Gaby <145401032+gjmessino@users.noreply.github.com>
2026-07-07 20:01:58 -0700

    fixed mistakes in first half of project 1

--- assignments_01/project_01.py
+++ assignments_01/project_01.py
@@ -7,7 +7,6 @@ from scipy import stats
 ## Task 1: Load Multiple Years of Data
 @task(retries=3, retry_delay_seconds=2)
 def load_data():
-    logger = get_run_logger()
     base_url = "https://raw.githubusercontent.com/Code-the-Dream-School/python-200-v1/refs/heads/main/assignments/resources/happiness_project/world_happiness_20"
     num = 15
     results=[]
@@ -21,21 +20,19 @@ def load_data():
         num+=1
     df = pd.concat(results, ignore_index=True)
     df.to_csv("assignments_01/outputs/merged_happiness.csv")
-    logger.info("____ Loading Data Complete ____")
     return df
 
 ## Task 2: Descriptive Statistics
 @task
 def happiness_scores(df):
     logger = get_run_logger()
-    logger.info(f"Mean: {df.mean()}")
-    logger.info(f"Median: {df.Median()}")
-    logger.info(f"Standard Deviation: {df.std()}")
-    yearly = df.group_by('year').agg({'Happiness score':'mean'})
-    regionally = df.group_by('region').agg({'Happiness score':'mean'})
+    logger.info(f"Mean: {df['Happiness score'].mean()}")
+    logger.info(f"Median: {df['Happiness score'].median()}")
+    logger.info(f"Standard Deviation: {df['Happiness score'].std()}")
+    yearly = df.groupby('year').agg({'Happiness score':'mean'})
+    regionally = df.groupby('Regional indicator').agg({'Happiness score':'mean'})
     logger.info(f"Yearly Mean: {yearly}")
     logger.info(f"Regional Mean: {regionally}")
-    logger.info("____ descriptive stats complete ____")
 
 ## Task 3: Visual Exploration
 @task
@@ -44,39 +41,48 @@ def make_visuals(df):
 
     plt.figure(1)
     plt.hist(df['Happiness score'])
+    plt.title('Happiness Score Histogram')
     plt.savefig("assignments_01/outputs/happiness_histogram.png")
-    plt.show()
+    plt.close()
     logger.info("Histogram")
 
     plt.figure(2)
     sns.boxplot(x = df['year'], y = df['Happiness score'])
+    plt.xlabel("Year")
+    plt.ylabel("Happiness Score")
+    plt.title("Happiness by Year")
     plt.savefig("assignments_01/outputs/happiness_by_year.png")
-    plt.show()
+    plt.close()
     logger.info("Boxplot")
 
     plt.figure(3)
     plt.scatter(df['GDP per capita'], df['Happiness score'])
+    plt.xlabel("GDP per Capita")
+    plt.ylabel("Happiness Score")
+    plt.title("Happiness based on GDP")
     plt.savefig("assignments_01/outputs/gdp_vs_happiness.png")
-    plt.show()
+    plt.close()
     logger.info("Scatter")
 
-    numeric_df = df.corr(method = 'peason', numeric_only=True)
+    numeric_df = df.corr(method = 'pearson', numeric_only=True)
 
     plt.figure(4)
     sns.heatmap(numeric_df, annot=True)
+    plt.title("Happiness Heatmap")
     plt.savefig("assignments_01/outputs/correlation_heatmap.png")
-    plt.show()
+    plt.close()
     logger.info("Heatmap")
-
-    logger.info('____ VIsializations complete ____')
 
 ## Task 4: Hypothesis Testing
 @task
 def hypo_testing(df):
     logger = get_run_logger()
-    before = df.loc[df['year'] == 2019, 'Happiness score']
-    after = df.loc[df['year'] == 2020, 'Happiness score']
+    df_2019 = df.loc[df['year'] = 2019, ['Country', 'Happiness score']]
+    df_2020 = df.loc['year' = 2020].dropna()
+    before = pd.Series(df_2019, index='Country')
+    after = pd.Series(df_2020, index='Country')
     tstat,pval = stats.ttest_rel(before, after)
+    logger.info(df_2019['Country'])
     logger.info(f"Stats: {tstat}")
     logger.info(f"P Values: {pval}")
     logger.info(f"2019 Mean: {before.mean()}")
@@ -91,13 +97,11 @@ def hypo_testing(df):
         logger.info("Insignificant")
     ## ADD COMMENT
     ## ADD SECOND TEST
-    logger.info('____ Hypo testing complete____ ')
 
 ## Task 5: Correlation and Multiple Comparisons
 @task
 def corr_comparison(df):
     corr, pval = stats.pearsonr(df.select_dtypes(include='number'))
-    get_run_logger('____ Correlation complete )___')
 
 ## Task 6: Summary Report
 @flow
