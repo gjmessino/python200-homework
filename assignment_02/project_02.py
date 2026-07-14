be88ef6e51513f8fba8deb79cd07bb5da7576c98
Gaby <145401032+gjmessino@users.noreply.github.com>
2026-07-14 16:23:34 -0700

    Fixed mistakes in warmup_02, and added comments

--- assignment_02/project_02.py
+++ assignment_02/project_02.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from sklearn.model_selection import train_test_split
 import numpy as np
+from sklearn.metrics import mean_squared_error
 
 ## Task 1: Load and Explore
 df = pd.read_csv('assignment_02/student/student-mat.csv', sep=';')
@@ -30,11 +31,9 @@ print(f"Shape: {new_df.shape}")
 # to accurately predict test scores because those 
 # students never took the test.
 
-# cols = ['schoolsup', 'internet', 'higher', 'activities']
-# new_df[cols] = new_df[cols].map({'yes': 1, 'no': 0})
-# # new_df['schoolsup'] = new_df['schoolsup'].map({'yes': 1, 'no': 0})
-# # print(new_df['schoolsup'])
-# new_df['sex'] = new_df['sex'].replace(['F', 'M'], [1,0])
+cols = ['schoolsup', 'internet', 'higher', 'activities']
+new_df[cols] = new_df[cols].replace(['yes', 'no'], [1,0])
+new_df['sex'] = new_df['sex'].replace(['F', 'M'], [1,0])
 
 r, pval = stats.pearsonr(df['G3'], df['absences'])
 r2, pval2 = stats.pearsonr(new_df['absences'], new_df['G3'])
@@ -72,15 +71,19 @@ model = LinearRegression()
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 
-rmse = np.sqrt(np.mean(y_pred - y_test)**2)
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 r2 = model.score(x_test, y_test)
 
 print(f"Slope: {model.coef_[0]}")
 print(f"RMSE: {rmse}")
 print(f"R Squared: {r2}")
 
-# The slope shows that the more failures there are, the lower the grade is. 
-#ADD COMMENT LATER
+# The negative slope shows that the more 
+# failures there are, the lower the 
+# grade is. RMSE indicates that the model's 
+# grade predictions are off by just over 2.5 
+# (or around %12.5). And R2 shows the model 
+# is not doing a great job of predicting grades.
 
 ## Task 5: Build the Full Model
 feature_cols = ["age", "Medu", "Fedu", "traveltime", "studytime", "failures",
@@ -89,20 +92,36 @@ feature_cols = ["age", "Medu", "Fedu", "traveltime", "studytime", "failures",
 X = new_df[feature_cols].values
 y = new_df["G3"].values
 
-x_train, y_train, x_test, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 model = LinearRegression()
 model.fit(x_train,y_train)
 y_pred = model.predict(x_test)
 
-rmse = np.sqrt(np.mean(y_pred-y_test))
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 train_r2 = model.score(x_train, y_train)
 test_r2 = model.score(x_test, y_test)
 
 print(f"RMSE: {rmse}")
 print(f"Training R2: {train_r2}")
 print(f"Testing R2: {test_r2}")
+
+# Both testing and training R2s are much higher 
+# than in question 4, showing the model is doing a 
+# much better job at making predictions. Adding
+# more features gives the model more information 
+# about things that impact grades.
+
 for name, coef in zip(feature_cols, model.coef_):
     print(f"{name:12s}: {coef:+.3f}")
+
+# schoolsup was the largest determinant of grade, 
+# and I was surprised that it was negative, given 
+# it seems like support would help students get a 
+# higher grade. It could potential be because there 
+# are only binary responses.
+#
+# In a real scenario I would drop free time, activities, 
+# and abseces because they are the least likely to impact grades.
 
 ## Task 6: Evaluate and Summarize
 plt.figure(2)
@@ -122,7 +141,7 @@ model = LinearRegression()
 model.fit(x_train,y_train)
 y_pred = model.predict(x_test)
 
-rmse = np.sqrt(np.mean(y_pred-y_test)**2)
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 r2 = model.score(x_test, y_test)
 
 print(f"RMSE: {rmse}")
--- assignment_02/warmup_02.py
+++ assignment_02/warmup_02.py
@@ -1,6 +1,6 @@
 import numpy as np
-
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
@@ -22,7 +22,7 @@ print(f"Intercept: {model.intercept_}")
 ## scikit-learn Question 2
 x = np.array([10, 20, 30, 40, 50])
 print(f"X Shape: {x.shape}")
-x.reshape(-1,1)
+x = x.reshape(-1,1)
 print(f"New Shape: {x.shape}")
 
 # scikit expects two arguments for it to work. 
@@ -32,23 +32,17 @@ print(f"New Shape: {x.shape}")
 
 ## scikit-learn Question 3
 X_clusters, _ = make_blobs(n_samples=120, centers=3, cluster_std=0.8, random_state=7)
-fig, (ax1,ax2) = plt.subplots(1,2, figsize=(12,5))
 
-#Left Plot
-ax1.scatter(X_clusters[:,0], X_clusters[:,1], color = 'gray', s=60, alpha=0.7)
-ax1.set_title('Raw Data')
-ax1.set_xlabel('X')
-ax1.set_ylabel('Y')
-
-#Right Plot
 kmeans = KMeans(n_clusters=3, random_state=42)
 kmeans.fit(X_clusters)
+centers = kmeans.cluster_centers_
 labels = kmeans.predict(X_clusters)
 
-ax2.scatter(X_clusters[:,0], X_clusters[:,1], c=labels, cmap='viridis', s=60, alpha=0.7)
-ax2.set_title('Data Clusters')
-ax1.set_xlabel('X')
-ax1.set_ylabel('Y')
+plt.scatter(X_clusters[:,0], X_clusters[:,1], c=kmeans.labels_, cmap='viridis', s=60, alpha=0.7)
+plt.scatter(centers[:, 0], centers[:, 1], c='red', s=200, marker='X', label='Centers')
+plt.title('Data Clusters')
+plt.xlabel('X')
+plt.ylabel('Y')
 
 plt.tight_layout()
 plt.savefig('assignment_02/outputs/kmeans_clusters.png')
@@ -94,7 +88,7 @@ print(f"Slope: {model.coef_[0]}")
 print(f"Intercept: {model.intercept_}")
 
 cost_predict = model.predict(age_test)
-rmse = np.sqrt(np.mean(cost_predict - cost_test)**2)
+rmse = np.sqrt(mean_squared_error(cost_test, cost_predict))
 r2 = model.score(age_test, cost_test)
 
 print(f"Root Mean Squared Error: {rmse}")
@@ -114,6 +108,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 model_full = LinearRegression()
 model_full.fit(X_train, y_train)
 r2 = model_full.score(X_test, y_test)
+y_pred = model.predict(X_test)
 
 print(f"R Squared: {r2}")
 print("age coefficient:    ", model_full.coef_[0])
@@ -124,7 +119,7 @@ print("smoker coefficient: ", model_full.coef_[1])
 ## Linear Regression Question 5
 plt.figure(3)
 plt.scatter(X_train, y_train, color = 'blue', label='Actual')
-plt.plot(X_test, y_test, color = 'red', label='Predicted')
+plt.plot(X_train, y_pred, color = 'red', label='Predicted')
 plt.xlabel('Age')
 plt.ylabel('Cost')
 plt.legend(labels)
