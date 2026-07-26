05663fa05d0605903b6675c937f625b6c315da83
Gaby <145401032+gjmessino@users.noreply.github.com>
2026-07-25 22:39:43 -0700

    started warmup_04

---
+++ assignment_04/warmup_04.py
@@ -0,0 +1,131 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.metrics import (
+    roc_curve,
+    roc_auc_score,
+    RocCurveDisplay,
+    classification_report,
+    f1_score
+)
+import joblib
+
+os.makedirs("outputs", exist_ok=True)
+os.makedirs("models", exist_ok=True)
+
+# Synthetic dataset — binary classification, two informative features
+X, y = make_classification(
+    n_samples=1000,
+    n_features=10,
+    n_informative=4,
+    n_redundant=2,
+    random_state=42,
+)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+## --- Part 1: Warmup Exercises --- ###
+
+## --- ROC and AUC --- ##
+
+## --- ROC Question 1 --- ##
+logreg = LogisticRegression(max_iter=1000, random_state=42)
+logreg.fit(X_train, y_train)
+
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(X_train)
+x_test_scaled = scaler.transform(X_test)
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(x_train_scaled, y_train)
+
+log_probs = logreg.predict_proba(X_test)[:, 1]
+knn_probs = knn.predict_proba(x_test_scaled)[:,1]
+
+print("Logistic Regression")
+print(f"Probabilities: {logreg.predict_proba(X_test)[:, 1]}")
+print(f"AUC Score: {roc_auc_score(y_test, log_probs)}")
+
+print("KNN")
+print(f"Probabilities: {knn.predict_proba(x_test_scaled)[:,1]}")
+print(f"AUC Score: {roc_auc_score(y_test, knn_probs)}")
+
+# KNN has the higher AUC score (.9394), 
+# so it is the better option for separating 
+# and testing data. The probabilities for 
+# KNN were also more consistant.
+
+## --- ROC Question 2 --- ##
+fpr_lr, tpr_lr, thresholds_lr = roc_curve(y_test, log_probs)
+fpr_knn, tpr_knn, thresholds_knn = roc_curve(y_test, knn_probs)
+
+fig, ax = plt.subplots(figsize=(6, 5))
+RocCurveDisplay(fpr=fpr_lr, tpr=tpr_lr).plot(ax=ax, name = 'Logistic Regression (AUC Score: 0.706)')
+ax.plot([0, 1], [0, 1], linestyle="--", color="gray", label="Random classifier")
+ax.set_title("ROC Curve")
+RocCurveDisplay(fpr=fpr_knn, tpr=tpr_knn).plot(ax=ax, name='KNN (AUC Score: 0.9394)')
+ax.legend()
+plt.tight_layout()
+plt.savefig('outputs/roc_comparison.png')
+plt.show()
+
+# When TPR = .8, the KNN model has almost no flase 
+# positives. However, the logistric regression model 
+# has roughly a .7 FPR at the same spot. Which gives 
+# KNN the higher overall success rating, because 
+# it is right more than it's wrong.
+
+## --- ROC Question 3 --- ##
+best_f1 = 0
+best_fpr = 0
+best_tpr = 0
+best_thresh = 0
+
+for threshold, f, t in zip(thresholds_lr, fpr_lr, tpr_lr):
+    y_pred = (log_probs >= threshold).astype(int)
+    f1 = f1_score(y_test, y_pred)
+    if f1 > best_f1:
+        best_f1 = f1
+        best_fpr = f
+        best_tpr = t
+        best_thresh = threshold
+
+print(f"Best F1: {best_f1}")
+print(f"Best FPR: {best_fpr}")
+print(f"Best TPR: {best_tpr}")
+print(f"Best Threshold: {best_thresh}")
+
+# The best threshold is below a 0.5 (.2756). 
+# This is because .5 is an arbitrary number 
+# that has no baring on the real data. 
+# Thresholds beyond .5 are good 
+# for data that doesn't have a binary 
+# solution. Low thresholds are good when 
+# the consequences of a false negative are 
+# worse than those of a false positive.
+
+## --- GridSearchCV --- ##
+
+## --- GridSearch Question 1 --- ##
+pipeline = Pipeline([
+   ("scaler", StandardScaler()),
+   ("logreg", LogisticRegression(max_iter=1000))
+])
+
+param_grid = {
+    'C' : [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+}
+
+grid_search = GridSearchCV(
+    param_grid=param_grid,
+    cv=5,
+    scoring="roc_auc"
+)
