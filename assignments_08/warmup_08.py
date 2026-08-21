82509f01c0a26c0ff1724a1f5b13c19641845dd3
Gaby <145401032+gjmessino@users.noreply.github.com>
2026-08-21 14:38:11 -0700

    finished half of warmup_08

---
+++ assignments_08/warmup_08.py
@@ -0,0 +1,33 @@
+# ----- Cloud Concepts Question 1 ----- #
+# Cloud computing means using someone elses hardware for storage as opposed to your own. 
+# This is most helpful with large scale products that have too much data for one of even 
+# a few computers. With cloud computing businesses can rent space from other companies at 
+# a rate that's cheaper than building and hosting one's own hardware.
+
+# ----- Cloud Concepts Question 2 ----- #
+# Vertical scaling is making one system bigger to handle large information loads. Examples 
+# including adding more RAM, GPU, or CPU to a device to gain more storage. 
+# Horizontal scaling includes adding new machines and splitting work between them to handle 
+# large data loads. An example of horizontal scaling is creating a data center with hundreds 
+# of servers to handle large data.
+
+# Scenario 1 - This could be either vertical or horizontal because it's unclear what the 
+# intended method of scaling it is; either method could work depending on the desires of the company. 
+
+# Scenario 2 -- This is vertical scaling given that it's focused on giving one machine more memory.
+
+# Scenario 3 -- This is horizontal scaling because the goal is to split data between multiple machines
+
+# ----- Cloud Concepts Question 3 ----- #
+# Gmail (SaaS) -- GMail requires almost nothing from users, and is built run and maintained by Google.
+# Azure Virtual Machines (IaaS) -- Azure Virtual Machines allows users to do everything themselves, including operating systems, building environments and handle security updates.
+# AWS S3 (Simple Storage Service) (IaaS) -- AWS S3 let's user's do everything except for hardware. However, other iterations of AWS fit into other categories.
+# GitHub Codespaces (PaaS) -- Codespaces manages hardware while letting users generate code and other functionalities on their service
+# Snowflake (SaaS) -- Snowflake is completely managed by itself without requiring any infrastructure from the user
+# Supabase (BaaS) -- Supabase gives users building blocks for coding while handling the backend themselves
+
+# Software as a Service (SaaS) requires the least from users, because the third-party company handles front/back end as well as hardware. Dropbox allows file sharing for users, but the users are not required to build any soft/hardware to manage or maintain the product.
+# Platform as a Service (PaaS) sits between SaaS and IaaS because it gives users the ability to create and manage software, but hardware and infrastructure are handled by the company itself. Google App Engine allows users to create their own apps, while handling the hardware themselves.
+# Infrastructure as a Service (IaaS) gives the most freedom to users, allowing them to handle every level of the build. Azure Virtual Machines allows users to fully manage their own services.
+
+# ----- Cloud Concepts Question 4 ----- #
