29c12e684a2662568d68483b6a881ed34793d58e
Gaby <145401032+gjmessino@users.noreply.github.com>
2026-09-11 16:04:40 -0700

    finsihed first attempt at warmup_11 and pipeline_run

---
+++ assignments_11/outputs/pipeline_run.md
@@ -0,0 +1,4 @@
+The pipeline did not run on the first try. Instead of copying the exact code from the lesson plan I tried to write it on my own and tried to extract key/value pairs, which did not work. I also had to jostle around a few lines of code and make sure what I said the included variables were in a function we actually included. I eventually got all tasks to run cleanly.
+ADD SOMETHING LATER
+ADD SOMETHING LATER
+DO THIS LATER
--- assignments_11/warmup_11.py
+++ assignments_11/warmup_11.py
@@ -7,8 +7,8 @@
 # I would only use @flow if the temperature conversion was part of a larger set of tasks to complete in order.
 
 ## Prefect Question 2 ##
-# from prefect import task, flow, get_run_logger
-# @task(retries= 3,retry_delay_seconds=30)
+from prefect import task, flow, get_run_logger
+@task(retries= 3,retry_delay_seconds=30)
 
 ## Prefect Question 3 ##
 # From the Prefect UI you can click on the pipeline you desire on the side panel labeled "Flow", which will show if 
@@ -18,3 +18,28 @@
 # the bottom. In this instance we would look at transform under the correct pipeline.
 
 # ----- Production Patterns ----- #
+
+## Production Question 1 ##
+# With raise_for_status() the code will raise an exception for any error, where as if response.status_code != 200: 
+# print("error") will only raise an error if it's in the 200s or API errors. This means any other kind of error won't 
+# get flagged and the code will continue on. This can be a problem is 400 and 500 level errors aren't caught, and can 
+# lead to inaccurate data. In the case of 400 or 500 errors there can be empty or malformed responses.
+
+## Production Question 2 ##
+# In this scenario upsert would stop the code from processing data twice. If insert had been used an error would 
+# immediately be raised because the code would read the half of the data that's been processed as new and try to process 
+# it again. However, if two rows have the same key an error will be raised, which is precisely what you happen if you 
+# try to re-add rows that have already been processed. With upsert, the code will recognize the already processed data 
+# and update anything with matching keys without raising an error or trying to create a new row.
+
+## Production Question 3 ##
+@task
+def get_info(enrichment_records: list) -> None:
+    get_run_logger.INFO(len(enrichment_records))
+
+## Production Question 4 ##
+# An incremental check won't reprocess records that have already been processed. In my answer about upsert for Production
+# Question 2 I mentioned how upsert will still update existing rows if they get updated twice, but incremental 
+# processing checks first to make sure rows already exist so we don't have to upsert them more than once. So if there is 
+# a data set with 100 rows, half of which are processed, instead of going through all of them again we check for what is 
+# done so as not to waste time, energy or money.
